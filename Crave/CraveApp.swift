ce42afbd1dd32a01bfe304bec9cf1097832889ab
valguric <areabijoodschl@gmail.com>
2023-10-09 15:17:14 +0300

    Initial Commit

---
+++ Crave/CraveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraveApp.swift
+//  Crave
+//
+//  Created by jood on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CraveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
