86cd6ef2637901f533a0c68e55d75c167efcba38
Yara <s.yara.sultan@gmail.com>
2023-10-12 17:58:07 +0300

    Wheel Spinner View with Alert

--- Crave/ContentView.swift
+++ Crave/ContentView.swift
@@ -6,19 +6,59 @@
 //
 
 import SwiftUI
+import FortuneWheel
 
 struct ContentView: View {
+    
+    @State var displayAlert = false
+    @State var selectIndex = 0
+    
+    var cuisines = ["American", "Italian", "Asian", "Indian", "Khaleeji", "Mexican"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 50) {
+            Text("Spin The Wheel")
+                .font(.largeTitle)
+                .foregroundStyle(.orange)
+                .bold()
+            
+            ZStack(alignment: .top) {
+                ZStack(alignment: .center) {
+                    FortuneWheel(titles: cuisines, size: 320, onSpinEnd: { i in selectIndex = i
+                        displayAlert = true
+                    })
+                    
+                }
+                .alert(isPresented: $displayAlert) {
+                    Alert(
+                        title: Text("Your Cuisine is"),
+                        message: Text(" \(cuisines[selectIndex])!"),
+                        dismissButton: .default(Text("Got it")) {
+                            displayAlert = false
+                        })
+                    
+                }
+            }
+            HStack {
+                Text("Swipe to spin")
+                    .font(Font.system(.headline))
+            .foregroundStyle(.orange)
+                Image(systemName: "hand.draw")
+                    .font(Font.system(.largeTitle))
+                    .foregroundStyle(.orange)
+            }
+            
+
         }
-        .padding()
+        
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
+
